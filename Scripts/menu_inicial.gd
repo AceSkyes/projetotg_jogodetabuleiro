a369ddc6359da423b4248bccc97c1412b1dec6ee
Ace <60721322+AceSkyes@users.noreply.github.com>
2024-11-03 22:45:05 -0300

    Navegação
    Adicionados Menus Iniciais, bem como a navegação entre eles.

---
+++ Scripts/menu_inicial.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _ready() -> void:
+	pass 
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("sair_menu_inicial"):
+		get_tree().quit()
+	pass
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Cenas/MenuPrincipal.tscn")
+	pass 
